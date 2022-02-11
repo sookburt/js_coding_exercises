84718671bcdb400a8ad5c99021ae9a2ad8c45566
Sue Burt <sookburt@gmail.com>
2022-02-11 16:01:42 +0000

    Completed exercise003.getTotalSubjects and renamed variables

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,16 +1,22 @@
 function getSquares(nums) {
   if (nums === undefined) throw new Error("nums is required");
-  return nums.map(x => x*x);
+  return nums.map(n => n*n);
 }
 
 function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
-  return words.shift() + words.map(x => x.charAt(0).toUpperCase() + x.substring(1)).join("");
+  return words.shift() + words.map(word => word.charAt(0).toUpperCase() + word.substring(1)).join("");
 }
 
 function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  let count = 0;
+
+  people.forEach(person => {
+    count += person.subjects.length;
+  });
+
+  return count;
 }
 
 function checkIngredients(menu, ingredient) {
