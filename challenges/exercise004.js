dfb25a026ee0a1aa8e064dfdd6882d9a32a27b9d
Sue Burt <sookburt@gmail.com>
2022-02-11 19:28:21 +0000

    exercise004 refactored to use map and filter as appropriate

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,10 +1,9 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
 
-  const processedArray = [];
-  nums.forEach(function (num) {
+  const processedArray = nums.filter(function (num) {
     if (num < 1) {
-      processedArray.push(num);
+      return num;
     }
   });
   return processedArray;
@@ -14,10 +13,9 @@ function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
 
-  const filteredNames = [];
-  names.forEach(function (name) {
+  const filteredNames = names.filter(function (name) {
     if (name.startsWith(char)) {
-      filteredNames.push(name);
+      return name;
     }
   });
   return filteredNames;
@@ -26,10 +24,9 @@ function findNamesBeginningWith(names, char) {
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
 
-  const verbs = [];
-  words.forEach(function (word) {
+  const verbs = words.filter(function (word) {
     if (word.startsWith("to ")) {
-      verbs.push(word);
+      return word;
     }
   });
   return verbs;
@@ -38,10 +35,9 @@ function findVerbs(words) {
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
 
-  const ints = [];
-  nums.forEach(function (num) {
+  const ints = nums.filter(function (num) {
     if (Number.isInteger(num)) {
-      ints.push(num);
+      return num;
     }
   });
   return ints;
@@ -50,9 +46,8 @@ function getIntegers(nums) {
 function getCities(users) {
   if (!users) throw new Error("users is required");
 
-  const cities = [];
-  users.forEach(function (user) {
-    cities.push(user.data.city.displayName);
+  const cities = users.map(function (user) {
+    return user.data.city.displayName;
   });
   return cities;
 }
@@ -60,9 +55,8 @@ function getCities(users) {
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
 
-  const squareRoots = [];
-  nums.forEach(function (num) {
-    squareRoots.push(+(Math.sqrt(num)).toFixed(2));
+  const squareRoots = nums.map(function (num) {
+    return +(Math.sqrt(num)).toFixed(2);
   });
   return squareRoots;
 }
@@ -71,10 +65,9 @@ function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
 
-  const targetSentences = [];
-  sentences.forEach(function (sentence) {
+  const targetSentences = sentences.filter(function (sentence) {
     if (sentence.toLowerCase().includes(str.toLowerCase())) {
-      targetSentences.push(sentence);
+      return sentence;
     }
   });
   return targetSentences;
@@ -83,9 +76,8 @@ function findSentencesContaining(sentences, str) {
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
 
-  const longest = [];
-  triangles.forEach(function (triangle) {
-    longest.push(Math.max(...triangle));
+  const longest = triangles.map(function (triangle) {
+    return Math.max(...triangle);
   });
   return longest;
 }
