3d6330c6fbcc290cc53856451dfd6f8a5614c9c1
Sue Burt <sookburt@gmail.com>
2022-02-12 16:51:46 +0000

    Completed exercises005.count1sand0s and reverseNumber - may refactor later

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -1,17 +1,32 @@
 const findNextNumber = (nums, n) => {
   if (nums === undefined) throw new Error("nums is required");
   if (n === undefined) throw new Error("n is required");
-  // Your code here!
+  // TODO: return to this
 };
 
 const count1sand0s = str => {
   if (str === undefined) throw new Error("str is required");
-  // Your code here!
+  // Assumed only 1 or 0 in string.
+  let count0 = 0;
+  let count1 = 0;
+  str.split('').forEach(char => {
+    if(char === '0') { count0++ }
+    if(char === '1') { count1++ }
+  });
+  return {
+    1: count1,
+    0: count0
+  };
+  // not exactly a single line arrow function... TODO: revisit?
 };
 
 const reverseNumber = n => {
   if (n === undefined) throw new Error("n is required");
   // Your code here!
+  let str = n.toString();
+  let arr = str.split('');
+  let reversed = arr.reverse();
+  return Number(reversed.join(''));
 };
 
 const sumArrays = arrs => {
