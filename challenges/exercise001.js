63fbfe89e4ddf3d2f8c273fec86bce6eaab92c0d
Sue Burt <sookburt@gmail.com>
2022-02-17 09:29:48 +0000

    Completed refactor and tidy of exercise001

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -1,32 +1,21 @@
 function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
-  let builder = word.charAt(0).toLocaleUpperCase() + word.substr(1);
-  return builder;
+
+  return word.charAt(0).toLocaleUpperCase() + word.substr(1);
 }
 
 function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
-  // TODO: check to see if input strings contain alphabetical characters only (locale considerations?).
 
-  /* ASSUMPTIONS: 
-
-      - We're not required to handle input values that might consist of a compound names, such as Carrie Anne, Rees-Mogg or Bonham Carter.
-      - We're not required to handle input values that might consist of 'son of' names such as O'Connor, MacDonald or McDonald
-      - We're not required to handle input values that might contain a suffix, such as Jr or III .
-
-      // TODO: consider returning to allow for some of these inputs if time later (regex?)
-  */
-
-  let initialBuilder = firstName.charAt(0).toUpperCase() + "." + lastName.charAt(0).toUpperCase();
-  return initialBuilder;
+  return `${firstName.charAt(0).toUpperCase()}.${lastName.charAt(0).toUpperCase()}`;
 }
 
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (vatRate === undefined) throw new Error("vatRate is required");
-  if (Number.isNaN(originalPrice)) throw new Error("originalPrice is not a number");
-  if (Number.isNaN(vatRate)) throw new Error("vatRate is not a number");
+  if (Number.isNaN(parseInt(originalPrice))) throw new Error("originalPrice is not a number");
+  if (Number.isNaN(parseInt(vatRate))) throw new Error("vatRate is not a number");
 
   let amountToBeAdded = +(vatRate / 100 * originalPrice).toFixed(2);
   return (originalPrice + amountToBeAdded);
@@ -35,8 +24,8 @@ function addVAT(originalPrice, vatRate) {
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
-  if (Number.isNaN(originalPrice)) throw new Error("originalPrice is not a number");
-  if (Number.isNaN(reduction)) throw new Error("reduction is not a number");
+  if (Number.isNaN(parseInt(originalPrice))) throw new Error("originalPrice is not a number");
+  if (Number.isNaN(parseInt(reduction))) throw new Error("reduction is not a number");
 
   let reductionAmount = +(reduction / 100 * originalPrice).toFixed(2);
   return (originalPrice - reductionAmount);
@@ -44,7 +33,7 @@ function getSalePrice(originalPrice, reduction) {
 
 function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
-  // Assumed any padding expected to be included.
+  if (str.length < 1) throw new Error("str is empty");
 
   let stringSize = str.length;
   if (stringSize % 2 < 1) {
@@ -58,6 +47,8 @@ function getMiddleCharacter(str) {
 
 function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
+  if (word.length < 1) throw new Error("word is empty");
+
   // Assumed any padding expected to be included.
 
   return word.split("").reverse().join("");
@@ -65,28 +56,22 @@ function reverseWord(word) {
 
 function reverseAllWords(words) {
   if (words === undefined) throw new Error("words is required");
-  // Assumed any padding expected to be included.
+  if (words.length < 1) throw new Error("words is empty");
 
-  let returnArray = words.map(x => reverseWord(x));
-  return returnArray;
+  return words.map(x => reverseWord(x));
 }
 
 function countLinuxUsers(users) {
   if (users === undefined) throw new Error("users is required");
-  let count = 0;
-  users.forEach(x => {
-    if (x.type === "Linux") {
-      count++;
-    }
-  });
-  return count;
+  if (users.length < 1) throw new Error("users is empty");
+
+  return users.filter(x => x.type === "Linux").length;
 }
 
 function getMeanScore(scores) {
   if (scores === undefined) throw new Error("scores is required");
 
   const reducer = (prev, curr) => prev + curr;
-
   return +(scores.reduce(reducer) / scores.length).toFixed(2);
 
 }
@@ -96,17 +81,9 @@ function simpleFizzBuzz(n) {
 
   let divBy3 = n % 3 === 0;
   let divBy5 = n % 5 === 0;
-
-  if (divBy3 && divBy5) {
-    return "fizzbuzz";
-  }
-  if (divBy3) {
-    return "fizz";
-  }
-  if (divBy5) {
-    return "buzz";
-  }
-
+  if (divBy3 && divBy5) { return "fizzbuzz"; }
+  if (divBy3) { return "fizz"; }
+  if (divBy5) { return "buzz"; }
   return n;
 }
 
--- test/exercise001.test.js
+++ test/exercise001.test.js
@@ -12,6 +12,13 @@ const {
 } = require("../challenges/exercise001");
 
 describe("capitalize", () => {
+
+  test("that it throws an error if word parameter is undefined", () => {
+    expect(() => {
+      capitalize();
+    }).toThrow("word is required");
+  });
+
   test("returns a capitalized string", () => {
     expect(capitalize("hello")).toBe("Hello");
     expect(capitalize("the quick fox")).toBe("The quick fox");
@@ -21,15 +28,60 @@ describe("capitalize", () => {
   test("does nothing if the string is already capitalized", () => {
     expect(capitalize("Hello")).toBe("Hello");
   });
+
+  test("does nothing if the string is empty", () => {
+    expect(capitalize("")).toBe("");
+  });
 });
 
 describe("generateInitials", () => {
+
+  test("that it throws an error if firstName argument is undefined", () => {
+    expect(() => {
+      generateInitials(undefined, "Bonneville");
+    }).toThrow("firstName is required");
+  });
+
+  test("that it throws an error if lastName argument is undefined", () => {
+    expect(() => {
+      generateInitials("Frederic", undefined);
+    }).toThrow("lastName is required");
+  });
+
   test("returns the initials of a firstname and surname", () => {
     expect(generateInitials("Frederic", "Bonneville")).toBe("F.B");
+    expect(generateInitials("Tommy", "O'Connor")).toBe("T.O");
+    expect(generateInitials("Helena", "Bonham Carter")).toBe("H.B");
+
   });
 });
 
 describe("addVAT", () => {
+
+  test("that it throws an error if originalPrice argument is undefined", () => {
+    expect(() => {
+      addVAT(undefined, 20);
+    }).toThrow("originalPrice is required");
+  });
+
+  test("that it throws an error if vatRate argument is undefined", () => {
+    expect(() => {
+      addVAT(100, undefined);
+    }).toThrow("vatRate is required");
+  });
+
+  test("that it throws an error if originalPrice is not a number", () => {
+    expect(() => {
+      addVAT("hello", 20);
+    }).toThrow("originalPrice is not a number");
+  });
+
+  test("that it throws an error if vatRate is not a number", () => {
+    expect(() => {
+      addVAT(100, "hello");
+    }).toThrow("vatRate is not a number");
+  });
+
   test("adds a VAT of 20% to a price of 100", () => {
     expect(addVAT(100, 20)).toBe(120);
   });
@@ -45,9 +97,38 @@ describe("addVAT", () => {
   test("adds a VAT of 0% to a price of 25", () => {
     expect(addVAT(25, 0)).toBe(25);
   });
+  
+  test("adds a VAT of 0% to a price of 0", () => {
+    expect(addVAT(0, 0)).toBe(0);
+  });
 });
 
 describe("getSalePrice", () => {
+
+  test("that it throws an error if originalPrice argument is undefined", () => {
+    expect(() => {
+      getSalePrice(undefined, 20);
+    }).toThrow("originalPrice is required");
+  });
+
+  test("that it throws an error if reduction argument is undefined", () => {
+    expect(() => {
+      getSalePrice(100, undefined);
+    }).toThrow("reduction is required");
+  });
+
+  test("that it throws an error if originalPrice is not a number", () => {
+    expect(() => {
+      getSalePrice("hello", 20);
+    }).toThrow("originalPrice is not a number");
+  });
+
+  test("that it throws an error if reduction is not a number", () => {
+    expect(() => {
+      getSalePrice(100, "hello");
+    }).toThrow("reduction is not a number");
+  });
+
   test("reduces a price of 100 by 50%", () => {
     expect(getSalePrice(100, 50)).toBe(50);
   });
@@ -63,19 +144,65 @@ describe("getSalePrice", () => {
   test("reduces a price of 50 by 0%", () => {
     expect(getSalePrice(50, 0)).toBe(50);
   });
+  
+  test("does nothing if the original price and reduction are both 0", () => {
+    expect(getSalePrice(0, 0)).toBe(0);
+  });
 });
 
 describe("getMiddleCharacter", () => {
+
+  test("that it throws an error if str argument is undefined", () => {
+    expect(() => {
+      getMiddleCharacter();
+    }).toThrow("str is required");
+  });
+
+  test("that it throws an error if str argument is empty", () => {
+    expect(() => {
+      getMiddleCharacter("");
+    }).toThrow("str is empty");
+  });
+
   test("returns the middle character from a string of odd length", () => {
     expect(getMiddleCharacter("bears!!!!")).toBe("s");
+  });
+
+  test("returns the middle character from a string of odd length including padding", () => {
+    expect(getMiddleCharacter(" bears!!!! ")).toBe("s");
   });
 
   test("returns the middle 2 characters from a string of even length", () => {
     expect(getMiddleCharacter("help!!")).toBe("lp");
   });
+  
+  test("returns the middle 2 characters from a string of even length including padding", () => {
+    expect(getMiddleCharacter(" help!! ")).toBe("lp");
+  });
+
+  test("returns the middle character from a string of odd length with only 1 character", () => {
+    expect(getMiddleCharacter("s")).toBe("s");
+  });
+
+  test("returns the middle 2 characters from a string of even length of only two characters", () => {
+    expect(getMiddleCharacter("lp")).toBe("lp");
+  });
 });
 
 describe("reverseWord", () => {
+
+  test("that it throws an error if word argument is undefined", () => {
+    expect(() => {
+      reverseWord();
+    }).toThrow("word is required");
+  });
+
+  test("that it throws an error if word argument is empty", () => {
+    expect(() => {
+      reverseWord("");
+    }).toThrow("word is empty");
+  });
+
   test("returns the provided word, reversed", () => {
     expect(reverseWord("foo")).toBe("oof");
   });
@@ -85,11 +212,34 @@ describe("reverseWord", () => {
       "?siht od ot tnaw neve uoy dluow yhw"
     );
   });
+
+  test("returns a single character if a single character was sent in.", () => {
+    expect(reverseWord("f")).toBe("f");
+  });
+
 });
 
+
 describe("reverseAllWords", () => {
+
+  test("that it throws an error if words argument is undefined", () => {
+    expect(() => {
+      reverseAllWords();
+    }).toThrow("words is required");
+  });
+
+  test("that it throws an error if worsd argument is empty", () => {
+    expect(() => {
+      reverseAllWords("");
+    }).toThrow("words is empty");
+  });
+
   test("reverses a single word in an array", () => {
     expect(reverseAllWords(["jest"])).toEqual(["tsej"]);
+  });
+
+  test("reverses a single word in an array with padding", () => {
+    expect(reverseAllWords([" jest "])).toEqual([" tsej "]);
   });
 
   test("reverses a multiple words in an array", () => {
@@ -97,15 +247,45 @@ describe("reverseAllWords", () => {
       reverseAllWords(["jest", "mocha", "rspec", "jasmine", "selenium"])
     ).toEqual(["tsej", "ahcom", "cepsr", "enimsaj", "muineles"]);
   });
+
+  test("reverses a multiple words in an array with padding", () => {
+    expect(
+      reverseAllWords(["jest", " mocha ", "rspec", "jasmine", " selenium "])
+    ).toEqual(["tsej", " ahcom ", "cepsr", "enimsaj", " muineles "]);
+  });
+
 });
 
 describe("countLinuxUsers", () => {
+
+  test("that it throws an error if users argument is undefined", () => {
+    expect(() => {
+      countLinuxUsers();
+    }).toThrow("users is required");
+  });
+
+  test("that it throws an error if users argument is empty", () => {
+    expect(() => {
+      countLinuxUsers([]);
+    }).toThrow("users is empty");
+  });
+
   test("returns 0 if no Linux users found", () => {
     const users = [
       { name: "Heather", OS: "Windows 8", type: "Windows" },
       { name: "Paul", OS: "Firefox OS", type: "Unknown" },
       { name: "Sheila", OS: "Windows 10", type: "Windows" },
       { name: "Pedro", OS: "Windows 95", type: "Windows" }
+    ];
+    expect(countLinuxUsers(users)).toBe(0);
+  });
+
+  test("returns 0 if no type property was found", () => {
+    const users = [
+      { name: "Heather", OS: "Windows 8", hello: "Windows" },
+      { name: "Paul", OS: "Firefox OS", hello: "Unknown" },
+      { name: "Sheila", OS: "Windows 10", hello: "Windows" },
+      { name: "Pedro", OS: "Windows 95", hello: "Windows" }
     ];
     expect(countLinuxUsers(users)).toBe(0);
   });
@@ -125,17 +305,33 @@ describe("countLinuxUsers", () => {
 });
 
 describe("getMeanScore", () => {
+
+  test("that it throws an error if scores argument is undefined", () => {
+    expect(() => {
+      getMeanScore();
+    }).toThrow("scores is required");
+  });
+
   test("returns the mean score from an array of scores", () => {
     expect(getMeanScore([8, 9, 7])).toBe(8);
     expect(getMeanScore([88, 86, 93])).toBe(89);
+    expect(getMeanScore([88, 88])).toBe(88);
   });
 
   test("returns the mean to 2 decimal places", () => {
     expect(getMeanScore([24, 44, 56, 11, 12, 17, 34])).toBe(28.29);
   });
+
 });
 
 describe("simpleFizzBuzz", () => {
+
+  test("that it throws an error if n argument is undefined", () => {
+    expect(() => {
+      simpleFizzBuzz();
+    }).toThrow("n is required");
+  });
+
   test("returns 'fizz' if the number is divisible by 3", () => {
     expect(simpleFizzBuzz(3)).toBe("fizz");
   });
@@ -151,4 +347,5 @@ describe("simpleFizzBuzz", () => {
   test("returns 'fizzbuzz' if the number is divisible by 3 and 5", () => {
     expect(simpleFizzBuzz(15)).toBe("fizzbuzz");
   });
+  
 });
