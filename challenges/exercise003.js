ac93e0842bed2de42d012d7770db6809bdf67467
Sue Burt <sookburt@gmail.com>
2022-02-18 16:26:32 +0000

    Added jsDoc comments and removed unnecessary comments from exercise003

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,13 +1,30 @@
+/**
+ * Takes an array of numbers and squares each then returns them in an Array.
+ * @param {Array} nums 
+ * @returns {Array}
+ */
 function getSquares(nums) {
   if (nums === undefined) throw new Error("nums is required");
+
   return nums.map(n => n * n);
 }
 
+/**
+ * Takes an array of words and returns them as a camel-cased concatenated String.
+ * @param {Array} words 
+ * @returns {String}
+ */
 function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
+
   return words.shift() + words.map(word => word.charAt(0).toUpperCase() + word.substring(1)).join("");
 }
 
+/**
+ * Takes an array of people objects and returns the number of subjects in the subjects property.
+ * @param {Array} people 
+ * @returns {Number}
+ */
 function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
   let count = 0;
@@ -17,6 +34,12 @@ function getTotalSubjects(people) {
   return count;
 }
 
+/**
+ * Checks if the ingredient is contained in the ingredients property of any of the menu objects.
+ * @param {Array} menu 
+ * @param {String} ingredient 
+ * @returns {Boolean}
+ */
 function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
@@ -32,14 +55,16 @@ function checkIngredients(menu, ingredient) {
   return ingredientExists;
 }
 
+/**
+ * Returns an array containing only the subset of numbers common to both input arrays.
+ * @param {Array} arr1 
+ * @param {Array} arr2 
+ * @returns {Array}
+ */
 function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
 
-  // filter first array to only those values included in second array
-  // drop into a new set to remove duplicates
-  // convert set to an array using ... spread operator then sort
-  // readability 0 : conciseness 1  
   return [...new Set(arr1.filter(num => arr2.includes(num)))].sort();
 }
 
