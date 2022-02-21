398c4a15a48675ce2835106287bf319520e4c1b5
Sue Burt <sookburt@gmail.com>
2022-02-21 15:25:21 +0000

    Reactored and added tests for exercise004

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,85 +1,100 @@
+/**
+ * Takes an array of numbers and returns an array of numbers < 1
+ * @param {Array} nums 
+ * @returns {Array}
+ */
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
 
-  const processedArray = nums.filter(function (num) {
-    if (num < 1) {
-      return num;
-    }
-  });
-  return processedArray;
+  return nums.filter(num => num < 1);
 }
 
+/**
+ * Filters names in the input array by those beginning with the character defined by the second parameter.
+ * @param {Array} names 
+ * @param {String} char 
+ * @returns {Array} 
+ */
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
 
-  const filteredNames = names.filter(function (name) {
-    if (name.startsWith(char)) {
-      return name;
-    }
-  });
-  return filteredNames;
+  return names.filter(name => name.startsWith(char));
 }
 
+/**
+ * Returns an array of verbs (that start with "to ") from the input array of words.
+ * @param {Array} words 
+ * @returns {Array}
+ */
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
 
-  const verbs = words.filter(function (word) {
-    if (word.startsWith("to ")) {
-      return word;
-    }
-  });
-  return verbs;
+  return words.filter(word => word.startsWith("to "));
 }
 
+/**
+ * From an input array of random numbers, returns only the Integers in a new array.
+ * @param {Array} nums 
+ * @returns {Array}
+ */
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
 
-  const ints = nums.filter(function (num) {
-    if (Number.isInteger(num)) {
-      return num;
-    }
-  });
-  return ints;
+  return nums.filter(num => Number.isInteger(num));
 }
 
+/**
+ * returns an array containing the displayName of the cities for each user in the input array
+ * @param {Array} users 
+ * @returns {Array}
+ */
 function getCities(users) {
   if (!users) throw new Error("users is required");
+  if (users.filter(user => user.data === undefined).length > 0) throw new Error("users.data is required");
+  if (users.filter(user => user.data.city === undefined).length > 0) throw new Error("users.data.city is required");
+  if (users.filter(user => user.data.city.displayName === undefined).length > 0) throw new Error("users.data.city.displayName is required");
 
-  const cities = users.map(function (user) {
-    return user.data.city.displayName;
-  });
-  return cities;
+  return users.map(user => user.data.city.displayName);
 }
 
+/**
+ * Takes an array of numbers (integers) and returns an array of square roots for each number in the input array to 2 decimal places.
+ * @param {Array} nums 
+ * @returns {Array}
+ */
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
+  if (nums.filter(num => (Number.isNaN(parseInt(num)))).length > 0) throw new Error("each item must be a number");
 
-  const squareRoots = nums.map(function (num) {
-    return +(Math.sqrt(num)).toFixed(2);
-  });
-  return squareRoots;
+  return nums.map(num => +(Math.sqrt(num)).toFixed(2));
 }
 
+/**
+ * From an input array of sentences, we return an array of sentences matching the search keyword (str)
+ * @param {Array} sentences 
+ * @param {String} str 
+ * @returns {Array}
+ */
 function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
 
-  const targetSentences = sentences.filter(function (sentence) {
-    if (sentence.toLowerCase().includes(str.toLowerCase())) {
-      return sentence;
-    }
-  });
-  return targetSentences;
+  return sentences.filter(sentence => sentence.toLowerCase().includes(str.toLowerCase()));
 }
 
+/**
+ * returns an array of the longest side of each set of triangle sub-arrays
+ * @param {Array} triangles 
+ * @returns {Array}
+ */
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
+  if (triangles.length < 1) throw new Error("triangles is empty");
+  if (triangles.filter(triangle => triangle.length !== 3).length > 0) throw new Error("each sub-array in triangles should contain 3 numbers");
+  if (triangles.flat().filter(triangle => Number.isNaN(parseInt(triangle))).length > 0) throw new Error("each sub-array in triangles should only contain 3 numbers");
 
-  const longest = triangles.map(function (triangle) {
-    return Math.max(...triangle);
-  });
-  return longest;
+  return triangles.map(triangle => Math.max(...triangle));
 }
 
 module.exports = {
--- test/exercise004.test.js
+++ test/exercise004.test.js
@@ -10,6 +10,13 @@ const {
 } = require("../challenges/exercise004");
 
 describe("findSmallNums", () => {
+
+  test("that it throws an error when nums is undefined", () => {
+    expect(() => {
+      findSmallNums();
+    }).toThrow("nums is required");
+  });
+
   test("returns an array of numbers smaller than 1", () => {
     expect(findSmallNums([8, 1, 1.3, 0.9, 0.4, -1])).toEqual([0.9, 0.4, -1]);
     expect(findSmallNums([-7, -243])).toEqual([-7, -243]);
@@ -19,6 +26,20 @@ describe("findSmallNums", () => {
 });
 
 describe("findNamesBeginningWith", () => {
+
+  test("that it throws an error when names is undefined", () => {
+    expect(() => {
+      findNamesBeginningWith(undefined, "S");
+    }).toThrow("names is required");
+  });
+
+  test("that it throws an error when char is undefined", () => {
+    expect(() => {
+      const names = ["Sally", "Dave", "Susan", "Geoff", "Riley", "Sam"];
+      findNamesBeginningWith(names, undefined);
+    }).toThrow("char is required");
+  });
+
   test("returns an array of names beginning with the specified character", () => {
     const names = ["Sally", "Dave", "Susan", "Geoff", "Riley", "Sam"];
     expect(findNamesBeginningWith(names, "S")).toEqual([
@@ -28,10 +49,18 @@ describe("findNamesBeginningWith", () => {
     ]);
     expect(findNamesBeginningWith(names, "D")).toEqual(["Dave"]);
     expect(findNamesBeginningWith(names, "F")).toEqual([]);
+    expect(findNamesBeginningWith([], "S")).toEqual([]);
   });
 });
 
 describe("findVerbs", () => {
+
+  test("that it throws an error when words is undefined", () => {
+    expect(() => {
+      findVerbs();
+    }).toThrow("words is required");
+  });
+
   test("returns an array of words that are considered verbs (because they begin with 'to ')", () => {
     const words = [
       "to eat",
@@ -59,6 +88,13 @@ describe("findVerbs", () => {
 });
 
 describe("getIntegers", () => {
+
+  test("that it throws an error when nums is undefined", () => {
+    expect(() => {
+      getIntegers();
+    }).toThrow("nums is required");
+  });
+
   test("returns an array containing only integers", () => {
     const nums = [1, 3.5, 2.1, 1, 4, 9];
     expect(getIntegers(nums)).toEqual([1, 1, 4, 9]);
@@ -68,6 +104,53 @@ describe("getIntegers", () => {
 });
 
 describe("getCities", () => {
+
+  test("that it throws an error when users is undefined", () => {
+    expect(() => {
+      getCities();
+    }).toThrow("users is required");
+  });
+
+  test("that it throws an error when users.data is undefined", () => {
+    expect(() => {
+      const users = [
+        {
+          id: 12
+        }
+      ];
+      getCities(users);
+    }).toThrow("users.data is required");
+  });
+
+  test("that it throws an error when users.city is undefined", () => {
+    expect(() => {
+      const users = [
+        {
+          id: 12,
+          data: {
+          }
+        }
+      ];
+      getCities(users);
+    }).toThrow("users.data.city is required");
+  });
+
+  test("that it throws an error when users.data.city.displayName is undefined", () => {
+    expect(() => {
+      const users = [
+        {
+          id: 12,
+          data: {
+            city: {
+              id: 1
+            }
+          }
+        }
+      ];
+      getCities(users);
+    }).toThrow("users.data.city.displayName is required");
+  });
+
   test("returns an array of the cities of each user", () => {
     const users = [
       {
@@ -112,6 +195,22 @@ describe("getCities", () => {
 });
 
 describe("getSquareRoots", () => {
+
+  test("that it throws an error when nums is undefined", () => {
+    expect(() => {
+      getSquareRoots();
+    }).toThrow("nums is required");
+  });
+
+  test("that it throws an error if any item in the input array is not a number", () => {
+    expect(() => {
+      let nums = [1, 2, "fluffy"];
+      getSquareRoots(nums);
+      nums = [1, 2, 5.2];
+      getSquareRoots(nums);
+    }).toThrow("each item must be a number");
+  });
+
   test("gets the square root of each number to 2 decimal places", () => {
     const nums = [36, 77, 12, 355, 92, 5];
     expect(getSquareRoots(nums)).toEqual([6, 8.77, 3.46, 18.84, 9.59, 2.24]);
@@ -119,6 +218,19 @@ describe("getSquareRoots", () => {
 });
 
 describe("findSentencesContaining", () => {
+
+  test("that it throws an error when sentences is undefined", () => {
+    expect(() => {
+      findSentencesContaining(undefined, "hello");
+    }).toThrow("sentences is required");
+  });
+
+  test("that it throws an error when str is undefined", () => {
+    expect(() => {
+      findSentencesContaining(["Hello, how are you", "I'm good thanks!"], undefined);
+    }).toThrow("str is required");
+  });
+
   const sentencesAboutPackageJson = [
     "You should specify a license for your package so that people know how they are permitted to use it",
     "The main field is a module ID that is the primary entry point to your program",
@@ -155,6 +267,33 @@ describe("findSentencesContaining", () => {
 });
 
 describe("getLongestSides", () => {
+
+  test("that it throws an error when triangles is undefined", () => {
+    expect(() => {
+      getLongestSides();
+    }).toThrow("triangles is required");
+  });
+
+  test("that it throws an error when triangles is empty", () => {
+    expect(() => {
+      getLongestSides([]);
+    }).toThrow("triangles is empty");
+  });
+
+  test("that it throws an error when any of the sub-arrays do not contain 3 numbers", () => {
+    expect(() => {
+      getLongestSides([[], [1, 2, 3]]);
+      getLongestSides([[1, 2, 3, 4, 5], [1, 2, 3]]);
+    }).toThrow("each sub-array in triangles should contain 3 numbers");
+  });
+
+  test("that it throws an error when any of the sub-arrays contain non numbers", () => {
+    expect(() => {
+      getLongestSides([["fluffy", 2, 3], [1, 2, 3]]);
+      getLongestSides([[1, 2, "fluffy"], [1, 2, 3]]);
+    }).toThrow("each sub-array in triangles should only contain 3 numbers");
+  });
+
   test("returns the longest side of each set of triangle data", () => {
     const data = [[6, 7, 10], [9, 3, 6], [6, 3, 5], [6, 13, 12], [7, 12, 8]];
     expect(getLongestSides(data)).toEqual([10, 9, 6, 13, 12]);
