8e4bc997bdaa73cc742c5e083bac08ebda97d6eb
Sue Burt <sookburt@gmail.com>
2022-02-21 17:28:03 +0000

    Started to refactor and add tests for exercise005

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -1,50 +1,53 @@
+/**
+ * returns the next number in the array after the first instance of the given number or null
+ * @param {Array} nums 
+ * @param {Number} n 
+ * @returns {Number}
+ */
 const findNextNumber = (nums, n) => {
   if (nums === undefined) throw new Error("nums is required");
   if (n === undefined) throw new Error("n is required");
-  let flag = false;
-  let target = null;
+  if (Number.isNaN(parseInt(n))) throw new Error("n must be a number");
+  if (nums.filter(num => Number.isNaN(parseInt(num))).length > 0) throw new Error("all values in the array should be a number");
 
-  nums.forEach(num => {
-    if (flag) {
-      target = num;
-      flag = false;
-    }
-    if (target === null && num === n) {
-      flag = true;
-    }
-  });
-
-  return target;
+  // TODO: return to this later - there ought to be a better way - this is rather unreadable.
+  return nums.indexOf(n) > -1 ? ((nums[nums.indexOf(n)+1] !== undefined) ? nums[nums.indexOf(n)+1] : null) : null;
 };
 
+/**
+ * Returns an object containing the count for the number of 1s and 0s in the incoming string.
+ * @param {String} str 
+ * @returns {Object}
+ */
 const count1sand0s = str => {
   if (str === undefined) throw new Error("str is required");
-  // Assumed only 1 or 0 in string.
-  let count0 = 0;
-  let count1 = 0;
-  str.split('').forEach(char => {
-    if (char === '0') { count0++ }
-    if (char === '1') { count1++ }
-  });
-  return {
-    1: count1,
-    0: count0
-  };
-  // not exactly a single line arrow function... TODO: revisit?
+
+  return { "0" : str.split("").filter(n => n === '0').length, "1": str.split("").filter(n => n === '1').length };
 };
 
+/**
+ * Reverses the numbers sent in and returns the number
+ * @param {Number} n 
+ * @returns {Number}
+ */
 const reverseNumber = n => {
   if (n === undefined) throw new Error("n is required");
-  // TODO: refactor.
-  let str = n.toString();
-  let arr = str.split('');
-  let reversed = arr.reverse();
-  return Number(reversed.join(''));
+  if (Number.isNaN(parseInt(n))) throw new Error("n must be a number");
+
+  return parseInt(n.toString().split('').reverse().join(''));
 };
 
+/**
+ * Takes a two dimensional array and returns the total of all numbers in the sub arrays.
+ * @param {Arrays} arrs 
+ * @returns {Number}
+ */
 const sumArrays = arrs => {
   if (arrs === undefined) throw new Error("arrs is required");
-  // assume only one level of flat array depth required.
+  //if ((arrs.flat().filter(n => Number.isNaN(parseInt(n)))).length > 0) throw new Error("a 2-D array of numbers is required");
+
+  // TO HERE: parseInt doesn't handle an array quite how you might expect parseInt([2, 4]) === 2 rather than NaN...
+  
   return (arrs.flat()).reduce((curr, prev) => curr + prev);
 };
 
--- test/exercise005.test.js
+++ test/exercise005.test.js
@@ -9,6 +9,31 @@ const {
 } = require("../challenges/exercise005");
 
 describe("findNextNumber", () => {
+
+  test("that it throws an error when nums is undefined", () => {
+    expect(() => {
+      findNextNumber(undefined, 3);
+    }).toThrow("nums is required");
+  });
+  
+  test("that it throws an error when n is undefined", () => {
+    expect(() => {
+      findNextNumber([2, 4, 6], undefined);
+    }).toThrow("n is required");
+  });
+
+  test("that it throws an error when n is not a number", () => {
+    expect(() => {
+      findNextNumber([2, 4, 6], "fluffy");
+    }).toThrow("n must be a number");
+  });
+
+  test("that it throws an error when nums contains a value that is not a number", () => {
+    expect(() => {
+      findNextNumber([2, "fluffy", 6], 2);
+    }).toThrow("all values in the array should be a number");
+  });
+
   test("returns the next number after the given number in the array", () => {
     expect(findNextNumber([5, 3, 7, 8, 1, 10], 7)).toBe(8);
     expect(findNextNumber([5, 3, 7, 8, 1, 10], 1)).toBe(10);
@@ -17,6 +42,8 @@ describe("findNextNumber", () => {
 
   test("if the number is not found in the array, returns null", () => {
     expect(findNextNumber([5, 3, 7, 8, 1, 10], 55)).toBe(null);
+    expect(findNextNumber([], 55)).toBe(null);
+
   });
 
   test("if the number is found more than once, returns the number after the first instance", () => {
@@ -29,6 +56,13 @@ describe("findNextNumber", () => {
 });
 
 describe("count1sand0s", () => {
+
+  test("that it throws an error when str is undefined", () => {
+    expect(() => {
+      count1sand0s();
+    }).toThrow("str is required");
+  });
+
   test("returns an object with the count of 1s and 0s in a string", () => {
     expect(count1sand0s("11000")).toEqual({
       1: 2,
@@ -36,6 +70,11 @@ describe("count1sand0s", () => {
     });
 
     expect(count1sand0s("0101010111")).toEqual({
+      1: 6,
+      0: 4
+    });
+
+    expect(count1sand0s("010F1010111")).toEqual({
       1: 6,
       0: 4
     });
@@ -53,15 +92,42 @@ describe("count1sand0s", () => {
 });
 
 describe("reverseNumber", () => {
+
+  test("that it throws an error when n is undefined", () => {
+    expect(() => {
+      reverseNumber();
+    }).toThrow("n is required");
+  });
+
+  test("that it throws an error when n is not a number", () => {
+    expect(() => {
+      reverseNumber("fluffy");
+    }).toThrow("n must be a number");
+  });
+
   test("reverses the digits of a number", () => {
     expect(reverseNumber(5)).toBe(5);
     expect(reverseNumber(104)).toBe(401);
     expect(reverseNumber(12345)).toBe(54321);
-    expect(reverseNumber(100)).toBe(1); // No leading 0 necessary
+    expect(reverseNumber(100)).toBe(1); 
   });
 });
 
 describe("sumArrays", () => {
+
+  test("that it throws an error when arrs is undefined", () => {
+    expect(() => {
+      sumArrays();
+    }).toThrow("arrs is required");
+  });
+
+  // test("that it throws an error when arrs is greater than a 2 dimensional array", () => {
+  //   expect(() => {
+  //     const arrs = [[1, [2, 4], 3], [6, 3, 1], [1], [9, 10], [3, 5]];
+  //     sumArrays(arrs);
+  //   }).toThrow("a 2-D array of numbers is required");
+  // });
+
   test("returns the total of the numbers in all sub arrays", () => {
     const arrs = [[1, 2, 3], [6, 3, 1], [1], [9, 10], [3, 5]];
     expect(sumArrays(arrs)).toBe(44);
