7c73d7b95208db9007f05448a3b6449e64955a5f
Sue Burt <sookburt@gmail.com>
2022-02-22 22:10:59 +0000

    Completed exercise008 and upgraded eslint
    added configuration to eslintrc to allow for optional chaining in ecma 2020

--- challenges/exercise008-bonus-activity.js
+++ challenges/exercise008-bonus-activity.js
@@ -1,32 +1,59 @@
 // Spread Syntax Bonus activity
 
-function printColours(colours){
-    if (!colours) throw new Error("colour is required");
-    // Your solution using the spread operator here!
+/**
+ * returns a list of colours
+ * @param {Array} colours 
+ * @return {Array}
+ */
+function printColours(colours) {
+    if (!colours) throw new Error("colours is required");
+
+    return [...colours];
 }
 
-function shoppingList(list, newItem){
+/**
+ * returns a list with an extra item(s) added
+ * @param {Array} list 
+ * @param {Array} newItem 
+ */
+function shoppingList(list, newItem) {
     if (!list) throw new Error("list is required");
     if (!newItem) throw new Error("newItem is required");
-    // Your solution using the spread operator here!
+
+    return [...list, ...newItem];
 }
 
-function highestNumber(numbers){
+/**
+ * returns the highest number from the input array
+ * @param {Array} numbers 
+ * @returns {Number}
+ */
+function highestNumber(numbers) {
     if (!numbers) throw new Error("numbers are required");
-    // Your solution using the spread operator here!
+
+    return Math.max(...numbers);
 }
 
-function splitThatString(string){
+/**
+ * returns a string split up into an Array
+ * @param {String} string 
+ * @return {Array}
+ */
+function splitThatString(string) {
     if (!string) throw new Error("string is required");
-    // Your solution using the spread operator here!
+
+    return [...string];
 }
 
-
-// Optional Chaining Bonus activity!
-
-function addressLookUp(user){
+/**
+ * returns a users postcode or undefined if the property doesn't exist
+ * @param {Object} user 
+ * @return {String}
+ */
+function addressLookUp(user) {
     if (!user) throw new Error("user is required");
-    // Your solution using optional chaining here!
+
+    return user.address?.postcode;
 }
 
 
--- test/exercise008-bonus-activity.test.js
+++ test/exercise008-bonus-activity.test.js
@@ -7,14 +7,38 @@ const {
 } = require("../challenges/exercise008-bonus-activity");
 
 
-describe("colours", () => {
+describe("printColours", () => {
+
+    test("that it throws an error if colours is undefined", () => {
+        expect(() => {
+            printColours();
+        }).toThrow("colours is required");
+    });
+
     test("returns a list of colours", () => {
-        const colours = ["red", "orange", "green"];
+        let colours = ["red", "orange", "green"];
+        expect(printColours(colours)).toEqual(colours);
+        colours = [];
         expect(printColours(colours)).toEqual(colours);
     });
 });
 
 describe("shoppingList", () => {
+
+    test("that it throws an error if list is undefined", () => {
+        expect(() => {
+            const list = ["one apple", "two dozen organic eggs", "cucumber", "bread"];
+            shoppingList(undefined, list);
+        }).toThrow("list is required");
+    });
+
+    test("that it throws an error if newItem is undefined", () => {
+        expect(() => {
+            const list = ["one apple", "two dozen organic eggs", "cucumber", "bread"];
+            shoppingList(list, undefined);
+        }).toThrow("newItem is required");
+    });
+
     test("returns a list with an extra item", () => {
         const list = ["one apple", "two dozen organic eggs", "cucumber", "bread"];
         const newItem = ["earl grey tea"]
@@ -29,6 +53,13 @@ describe("shoppingList", () => {
 });
 
 describe("highestNumber", () => {
+
+    test("that it throws an error if numbers is undefined", () => {
+        expect(() => {
+            highestNumber();
+        }).toThrow("numbers are required");
+    });
+
     test("returns the highest number", () => {
         const numbers = [12, 20, 18];
         expect(highestNumber(numbers)).toEqual(20);
@@ -36,20 +67,34 @@ describe("highestNumber", () => {
 });
 
 describe("splitThatString", () => {
+
+    test("that it throws an error if string is undefined", () => {
+        expect(() => {
+            splitThatString();
+        }).toThrow("string is required");
+    });
+
     test("returns a string split up", () => {
         const string = "USA";
         expect(splitThatString(string)).toEqual(["U", "S", "A"]);
     });
+
     test("returns a string split up", () => {
         const string = "123";
         expect(splitThatString(string)).toEqual(["1", "2", "3"]);
     });
 });
 
-
 // Optional Chaining 
 
 describe("addressLookUp", () => {
+    
+    test("that it throws an error if user is undefined", () => {
+        expect(() => {
+            addressLookUp();
+        }).toThrow("user is required");
+    });
+
     test("returns a users postcode", () => {
 
         const user = {
@@ -64,6 +109,7 @@ describe("addressLookUp", () => {
         }
         expect(addressLookUp(user)).toEqual('H12 333');
     });
+
     test("returns undefined if address isn't provided", () => {
 
         const user = {
